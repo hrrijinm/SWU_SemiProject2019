f0817c8da5e52db047ff0f7e652b267780237f71
KimHeeJin <kphj0111@naver.com>
2019-07-03 14:55:13 +0900

    첫 번째 나의 커밋이다.

--- app/src/main/java/com/example/swu_semiproject2019/MainActivity.java
+++ app/src/main/java/com/example/swu_semiproject2019/MainActivity.java
@@ -9,5 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //테스트코드1
+
+        //오늘 컨디션 중간~~
+
+        //커밋 및 push 테스트
     }
 }
